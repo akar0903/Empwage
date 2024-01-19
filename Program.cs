3dc01434c1be55a489a63fcfc050e9e1ee75e7b5
akar0903 <av0019@srmist.edu.in>
2024-01-19 17:22:21 +0530

    Use case 1: employee wage

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeewage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wage = Console.ReadLine();
+            string hour = Console.ReadLine();
+            Console.WriteLine((Convert.ToInt32(wage) * (Convert.ToInt32(hour))));
+            Console.ReadLine();
+        }
+    }
+}
