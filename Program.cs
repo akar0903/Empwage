7e692a8de5078553839ef7360a474194e6a23f89
akar0903 <av0019@srmist.edu.in>
2024-01-19 19:23:44 +0530

    Work wages and present using switch cases

--- Program.cs
+++ Program.cs
@@ -1,11 +1,38 @@
 ﻿using System;
-class UC4
+class HelloWorld
 {
     static void Main()
     {
-        string days = Console.ReadLine();
-        string wage = Console.ReadLine();
-        Console.WriteLine(Convert.ToInt32(days) * Convert.ToInt32(wage));
+        Random random = new Random();
+        int day = random.Next(0, 2);
+        String time = Console.ReadLine();
+        int duration = Convert.ToInt32(time);
+        switch (day)
+        {
+            case 0:
+                Console.WriteLine("Absent");
+                break;
+            case 1:
+                Console.WriteLine("Present");
+                break;
+            default:
+                Console.WriteLine("Invalid number");
+                break;
 
+        }
+        switch (duration)
+        {
+            case 4:
+                Console.WriteLine("Part time");
+                Console.WriteLine("Wage is " + 20 * 4);
+                break;
+            case 8:
+                Console.WriteLine("Full time");
+                Console.WriteLine("Wage is " + 20 * 8);
+                break;
+            default:
+                Console.WriteLine("Invalid working period");
+                break;
+        }
     }
 }
